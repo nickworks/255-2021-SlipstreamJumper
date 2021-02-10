58cbefb0785793362f14fe8b45d95820974fd086
William Velting <wvelting@gmail.com>
2021-02-10 14:00:28 -0500

    Beginning of Lab 1 - Player can move
    +Player Movement w/ Euler Physics

---
+++ Assets/_Velting/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Velting
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        public float acceleration = 20;
+        public float deceleration = 40;
+        private Vector3 velocity = new Vector3();
+        
+        void Start()
+        {
+
+        }
+
+       
+        void Update()
+        {
+
+            float h = Input.GetAxis("Horizontal");
+
+            // Euler physics integration:
+
+            if (h != 0) //user is pressing left or right (or both)
+            {
+                //applying acceleration to our velocity:
+                velocity.x += h * Time.deltaTime * acceleration;
+            }
+
+            else //user is not pushing left or right
+            {
+                if (velocity.x > 0) // player is moving right
+                {
+                    velocity.x += -deceleration * Time.deltaTime;
+                    if (velocity.x < 0) velocity.x = 0;
+                }
+
+                if (velocity.x < 0) //player is moving left 
+                {
+                    velocity.x += deceleration * Time.deltaTime;
+                    if (velocity.x > 0) velocity.x = 0;
+                }
+            }
+
+
+            
+            //applying our velocity to our position
+            transform.position += velocity * Time.deltaTime;
+
+        }
+    }
+}
