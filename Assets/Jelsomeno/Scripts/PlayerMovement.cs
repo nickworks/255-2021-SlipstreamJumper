f89dfb081a4d7f806a1190255354ef41ace8d885
Rocco <roccojelso77@gmail.com>
2021-02-10 14:57:58 -0500

    Lab Push 2
    Added jumping and gravity, also added more comments to code to try and get in the habit of doing so.

--- Assets/Jelsomeno/Scripts/PlayerMovement.cs
+++ Assets/Jelsomeno/Scripts/PlayerMovement.cs
@@ -3,12 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Jelsomeno {
+    /// <summary>
+    /// This class gets input and moves the player
+    /// with the input and Euler physics.
+    /// </summary>
 
     public class PlayerMovement : MonoBehaviour
     {
-        public float acceleration = 5;
-        public float deceleration = 40;
+        /// <summary>
+        /// When the player wants to move, this value is used to scale the players acceleration.
+        /// </summary>
+        [Header("Horizontal Movement")]
+        
+        public float scalerAcceleration = 5;
+
+        /// <summary>
+        /// This value is used to scale the players deceleration.
+        /// </summary>
+
+        public float scalerDeceleration = 40;
+
+        /// <summary>
+        /// This value is used to clamp the players horizontal velocity, measured in meters/second
+        /// </summary>
+
+        public float maxSpeed = 5;
+
+        /// <summary>
+        /// This is used to scale the players downward acceleration due to gravity.
+        /// </summary>
+
+        [Header("Vertical Movement")]
+
+        public float gravity = 10;
+
+        /// <summary>
+        /// The velocity we launch the player when they jump, measuered in meters/second
+        /// </summary>
+
+        public float jumpImpulse = 10;
+
+        /// <summary>
+        /// current velocity of the player in meters.second
+        /// </summary>
+
         private Vector3 velocity = new Vector3();
+
+        /// <summary>
+        /// Whether or not the player is currently jumping upwards.
+        /// </summary>
+
+        private bool isJumpingUpwards = false;
 
         // Start is called before the first frame update
         void Start()
@@ -19,7 +64,69 @@ namespace Jelsomeno {
         // Update is called once per frame
         void Update()
         {
+            CalcHorizontalMovement();
 
+            CalcVerticalMovement();
+
+            //applying velocity to position 
+            transform.position += velocity * Time.deltaTime;
+
+        }
+
+
+        /// <summary>
+        /// Calculating Euler physics on Y axis
+        /// </summary>
+
+        private void CalcVerticalMovement()
+        {
+
+
+            float gravMultiplier = 1;
+
+            // detect if on the ground:
+            bool isGrounded = false;
+            if (transform.position.y <= 0)
+            {
+
+                Vector3 pos = transform.position;
+                pos.y = 0;
+                transform.position = pos;
+                velocity.y = 0;
+                isGrounded = true;
+
+            }
+
+            bool wantsToJump = Input.GetButtonDown("Jump");
+
+            bool isHoldingJump = Input.GetButton("Jump");
+
+
+            if (wantsToJump && isGrounded)
+            {
+                velocity.y = jumpImpulse;
+                isJumpingUpwards = true;
+            }
+            if (!isHoldingJump || velocity.y < 0)
+            {
+                isJumpingUpwards = false;
+            }
+
+            if (isJumpingUpwards) gravMultiplier = 0.5f;
+
+
+            // apply force of gravity to our velocity 
+            velocity.y -= gravity * Time.deltaTime * gravMultiplier;
+
+
+        }
+
+        /// <summary>
+        /// Calculating Euler physics on X axis
+        /// </summary>
+
+        private void CalcHorizontalMovement()
+        {
             float h = Input.GetAxisRaw("Horizontal");
 
             // Euler physics integration:
@@ -27,28 +134,32 @@ namespace Jelsomeno {
             if (h != 0)
             {
                 // applying acceleration to velocity
-                velocity.x += h * Time.deltaTime * acceleration;
+                velocity.x += h * Time.deltaTime * scalerAcceleration;
 
-            } else {
+            }
+            else
+            {
 
-                if(velocity.x > 0) // player is moving right
+                if (velocity.x > 0) // player is moving right
                 {
-                    velocity.x -= deceleration * Time.deltaTime;
+                    velocity.x -= scalerDeceleration * Time.deltaTime;
                     if (velocity.x < 0) velocity.x = 0;
 
                 }
-                if(velocity.x < 0)
+                if (velocity.x < 0)
                 {
-                    velocity.x += deceleration * Time.deltaTime;
+                    velocity.x += scalerDeceleration * Time.deltaTime;
                     if (velocity.x > 0) velocity.x = 0;
 
                 }
 
             }
 
-            //applying velocity to position 
-            transform.position += velocity * Time.deltaTime;
+            //if (velocity.x < -maxSpeed) velocity.x = -maxSpeed;
+            //if (velocity.x >  maxSpeed) velocity.x = maxSpeed;
 
+            velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
         }
+
     }
 }
