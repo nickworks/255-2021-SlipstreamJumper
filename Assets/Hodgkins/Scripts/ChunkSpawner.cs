4ff8c04763103167412cdff72b2e9ca33de020f7
Tom H <darksonic3211@gmail.com>
2021-02-24 17:07:28 -0500

    Week 7 - Chunk, Hazard, new OverlapObject Class
    Added a class that includes the spring and a new hazard block, that can damage and kill the player. Also added scripts for chunk class and spawns.

--- Assets/Hodgkins/Scripts/ChunkSpawner.cs
+++ Assets/Hodgkins/Scripts/ChunkSpawner.cs
@@ -6,16 +6,26 @@ namespace Hodgkins
 {
     public class ChunkSpawner : MonoBehaviour
     {
-
-        public Transform prefab;
+        public Chunk prefab;
+        private List<Chunk> chunks = new List<Chunk>();
 
         void Start()
-        {
-            
+        {            
             for (int i = 0; i < 5; i++)
             {
-                float y = Random.Range(-2, 2f);
-                Instantiate(prefab, new Vector3(i * 16, y, 0), Quaternion.identity);
+                Vector3 pos = Vector3.zero;
+
+                if (chunks.Count > 0)
+                {
+                    Chunk lastChunk = chunks[chunks.Count - 1];
+                    pos = lastChunk.connectionPoint.position;
+                }
+
+                //float y = Random.Range(-2, 2f);
+                //pos.y += y;
+
+                Chunk newChunk = Instantiate(prefab, pos, Quaternion.identity);
+                chunks.Add(newChunk);
 
             }
         }
---
+++ Assets/Hodgkins/Scripts/OverlapObject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hodgkins
+{
+    public class OverlapObject : MonoBehaviour
+    {
+        AABB aabb;
+        // Start is called before the first frame update
+        void Start()
+        {
+            aabb = GetComponent<AABB>();
+            Zone.main.powerups.Add(aabb);
+        }
+
+        private void OnDestroy()
+        {
+            if (Zone.main == null) return; // do nothing
+            Zone.main.powerups.Remove(aabb);
+        }
+
+        /// <summary>
+        /// This function should be overridden by child classes (hence 'virtual')
+        /// </summary>
+        /// <param name="pm"></param>
+        virtual public void OnOverlap(PlayerMovement pm)
+        {
+            //pm.LaunchPlayer(new Vector3(0, 20, 0));
+        }
+    }
+}
