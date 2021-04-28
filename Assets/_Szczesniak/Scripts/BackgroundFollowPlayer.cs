f0d345d236dd95e83ac3b58afb32c2f20870ca55
Gavin Szczesniak <szczesg@ferris.edu>
2021-04-27 21:53:13 -0400

    New code documented
    * Commented code on the BackgroundFollowPlayer script

--- Assets/_Szczesniak/Scripts/BackgroundFollowPlayer.cs
+++ Assets/_Szczesniak/Scripts/BackgroundFollowPlayer.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Szczesniak {
+    /// <summary>
+    /// The background follows the camera to keep the player from seeing out of bounds
+    /// </summary>
     public class BackgroundFollowPlayer : MonoBehaviour {
 
-        public Transform playerPos;
+        /// <summary>
+        /// Gets camera transform
+        /// </summary>
+        public Transform Cam;
 
         void Update() {
-            Vector3 backgroundPos = new Vector3(playerPos.position.x, transform.position.y, transform.position.z);
-            transform.position = backgroundPos;
+            Vector3 backgroundPos = new Vector3(Cam.position.x, transform.position.y, transform.position.z); // gets information to move background to cam
+            transform.position = backgroundPos; // moves background to cam
             
         }
     }
